b25a2d41d03a5e1589e2a867756a11d14397359a
Mohammed Sadiq <mzainkofficial@gmail.com>
2022-05-31 23:29:45 +0530

    NavigationContainer and Navigator to App

--- App.js
+++ App.js
@@ -1,7 +1,17 @@
-import HomeScene from "./scenes/HomeScene/HomeScene";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import NotesScene from "./scenes/NotesScene/NotesScene";
 
-const App = () => {
-    return HomeScene();
+const { Navigator, Screen: Scene } = createNativeStackNavigator();
+
+const TuskyApp = () => {
+    return (
+        <NavigationContainer>
+            <Navigator initialRouteName="Notes">
+                <Scene name="Notes" component={NotesScene} />
+            </Navigator>
+        </NavigationContainer>
+    );
 };
 
-export default App;
+export default TuskyApp;
