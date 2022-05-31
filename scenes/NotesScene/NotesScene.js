2faf6da266e695400982fe1468ca10070316b3be
Mohammed Sadiq <mzainkofficial@gmail.com>
2022-05-31 23:28:08 +0530

    Rename HomeScene as NotesScene

---
+++ scenes/NotesScene/NotesScene.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const NotesScene = () => {
+    return <View style={styles.sceneStyle}></View>;
+};
+
+const styles = StyleSheet.create({
+    sceneStyle: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+});
+
+export default NotesScene;
