5e0e8e9caa5ac440e9a739c581e179b0a2e6ada7
Mohammed Sadiq <mzainkofficial@gmail.com>
2022-06-04 13:28:04 +0530

    Impose a basic structure on scenes folder

---
+++ scenes/viewNoteScene/ViewNoteScene.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const ViewNoteScene = () => {
+    return <View></View>;
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default ViewNoteScene;
